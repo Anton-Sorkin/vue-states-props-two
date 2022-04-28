202c7808d7d1d1e4bd4248fe8ad5d82e619c3d37
Filipengbergs <filip792@hotmail.com>
2022-04-28 15:09:52 +0200

    Second commit

---
+++ src/components/ObjectChild.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>{{ animalName }}</h2>
+    <p>{{ animalAge }}</p>
+    <img src="{{animalImage}}" alt="{{animalName}}" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+@Options({
+  components: {},
+})
+export default class ObjectChild extends Vue {
+  @Prop() animalName: string;
+  @Prop() animalAge: number;
+  @Prop() animalImage: string;
+  @Prop() animalGender: boolean;
+}
+</script>
+
+<style scoped lang="scss"></style>
